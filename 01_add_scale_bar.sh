b8c6ed8a6fdfe7c17fbb0aeff1c49f27062b9b73
Kristian Peters <kristian.peters@korseby.net>
2024-06-09 14:01:26 +0200

    updated script for reading tsv input

--- 01_add_scale_bar.sh
+++ 01_add_scale_bar.sh
@@ -3,22 +3,23 @@
 IFS="
 "
 
-FILELIST="_liverworts_photos_list.csv"
+FILELIST="_liverworts_photos_list.txt"
 
 echo "#!/usr/bin/env bash" > add_scale_bar_commands.sh
 chmod 755 add_scale_bar_commands.sh
 
-for i in $(find . -name '*.jpg' | sort); do
+for i in $(find ultra/ -name '*.jpg' | sort); do
 	INPUT="$i";
-	OUTPUT="$(echo $i | perl -pe 's/\.jpg/\.jpeg/')";
+	OUTPUT="$(echo $i | perl -pe 's/ultra/high/' | perl -pe 's/\.jpg/\.jpeg/')";
 	NAME="$(basename $i .jpg)";
-	OBJECTIVE="$(cat ${FILELIST} | grep ${NAME}\; | cut -d';' -f 8 | head -n 1)";
-	MICROSCOPE="$(cat ${FILELIST} | grep ${NAME}\; | cut -d';' -f 5 | head -n 1)";
-	MAGNIFICATION="$(cat ${FILELIST} | grep ${NAME}\; | cut -d';' -f 6 | head -n 1)";
+	CAMERA="$(cat ${FILELIST} | grep ${NAME}$'\t' | cut -d$'\t' -f 19 | head -n 1)";
+	OBJECTIVE="$(cat ${FILELIST} | grep ${NAME}$'\t' | cut -d$'\t' -f 8 | head -n 1)";
+	MICROSCOPE="$(cat ${FILELIST} | grep ${NAME}$'\t' | cut -d$'\t' -f 5 | head -n 1)";
+	MAGNIFICATION="$(cat ${FILELIST} | grep ${NAME}$'\t' | cut -d$'\t' -f 6 | head -n 1)";
 	if [[ "${MICROSCOPE}" == "Balgen" ]]; then
-		COMMAND="/Users/kristian/Desktop/liverworts/scale_bar/scale_bar.py -c /Users/kristian/Desktop/liverworts/scale_bar/scale_bar_distances.csv -i \"${INPUT}\" -o \"${OUTPUT}\" -s 0 -fms \"Bellow\" -fo \"${OBJECTIVE}\" -fmg \"${MAGNIFICATION}\"";
+		COMMAND="/Users/kristian/Desktop/liverworts/scale_bar/scale_bar.py -c /Users/kristian/Desktop/liverworts/scale_bar/scale_bar_distances.csv -i \"${INPUT}\" -o \"${OUTPUT}\" -s 0 -fc \"${CAMERA}\" -fms \"Bellow\" -fo \"${OBJECTIVE}\" -fmg \"${MAGNIFICATION}\"";
 	else
-		COMMAND="/Users/kristian/Desktop/liverworts/scale_bar/scale_bar.py -c /Users/kristian/Desktop/liverworts/scale_bar/scale_bar_distances.csv -i \"${INPUT}\" -o \"${OUTPUT}\" -s 0 -fo \"${OBJECTIVE}\" -fmg \"${MAGNIFICATION}\"";
+		COMMAND="/Users/kristian/Desktop/liverworts/scale_bar/scale_bar.py -c /Users/kristian/Desktop/liverworts/scale_bar/scale_bar_distances.csv -i \"${INPUT}\" -o \"${OUTPUT}\" -s 0 -fc \"${CAMERA}\" -fo \"${OBJECTIVE}\" -fmg \"${MAGNIFICATION}\"";
 	fi
 	echo "echo \"${NAME}\"\:" >> add_scale_bar_commands.sh
 	echo "${COMMAND}" >> add_scale_bar_commands.sh
